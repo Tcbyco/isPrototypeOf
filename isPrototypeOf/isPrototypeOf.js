dd757857ac66b34a08166892bf3395ecfd2f580e
Tcbyco <coy.toby@gmail.com>
2020-01-25 19:36:24 +1100

    Improved error handling
    Now throws TypeError if first argument (prototype) is undefined or null.

--- isPrototypeOf/isPrototypeOf.js
+++ isPrototypeOf/isPrototypeOf.js
@@ -1,6 +1,9 @@
 function isPrototypeOf(proto, obj) {
-  // javascript gotcha: typeof null === 'object'
-  if (!(typeof proto === 'object') || proto === null) {
+  if (proto === null || proto === undefined) {
+    throw new TypeError(`Cannot read property isPrototypeOf of ${proto}.`)
+  }
+
+  if (!(typeof proto === 'object')) {
     return false;
   } 
 
@@ -8,20 +11,20 @@ function isPrototypeOf(proto, obj) {
     return false;
   } 
 
-  // Recursive base case A: the prototype of Object.prototype === null.
+  // Recursive base case A
   // We arrive here if proto is not a prototype of obj.
   if (obj === null) {
     return false;
   }
 
-  // Recursive base case B
   // Compare prototype with the prototype of object.
+  // If the two are equal, this is Recursive base case B.
   if (proto === Object.getPrototypeOf(obj)) {
     return true; 
   } 
   
   // Recurse, passing in prototype and the prototype of object.
   // In other words, step up 1 link in the object's prototype chain
-  // and compare that to prototype.
+  // and compare that to proto.
   return isPrototypeOf(proto, Object.getPrototypeOf(obj));
 }
